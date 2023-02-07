daaf8a50d546fd6976c2f65f3ff3a5ac38321481
Mizan Choudhury <mizan.choudhury@greenzoneuk.com>
2023-02-07 14:53:30 +0000

    #6758 fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+
+	let a = 0;
+  let b = 1;
+  for (let i = 1; i < count; i++) {
+    const num = b;
+    b = a + b;
+    a = num;
+  }
+  return b;
 
 };
 

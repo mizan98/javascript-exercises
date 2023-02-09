edd436589cfb56be9304397567a88c1e5e80c7ec
Mizan Choudhury <mizan.choudhury@greenzoneuk.com>
2023-02-09 08:38:56 +0000

    #6763 ceaser cipher changes

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,8 @@
-const caesar = function() {
+const caesar = function(str, val) {
+	// let  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+	let alphabets =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+	let cipher = alphabets.includes(str)
+	return 
 
 };
 
